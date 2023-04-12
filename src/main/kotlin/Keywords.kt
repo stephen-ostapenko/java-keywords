c343a93b57e5551f4ab08ccd382b61b70ec4217e
Stephen Ostapenko <stepanos2002@gmail.com>
2023-04-12 11:56:03 +0300

    add saveStats

---
+++ src/main/kotlin/Keywords.kt
@@ -0,0 +1,69 @@
+object JavaKeywordConstants {
+    const val ABSTRACT = "abstract"
+    const val ASSERT = "assert"
+    const val BOOLEAN = "boolean"
+    const val BREAK = "break"
+    const val BYTE = "byte"
+    const val CASE = "case"
+    const val CATCH = "catch"
+    const val CHAR = "char"
+    const val CLASS = "class"
+    const val CONTINUE = "continue"
+    const val DEFAULT = "default"
+    const val DO = "do"
+    const val DOUBLE = "double"
+    const val ELSE = "else"
+    const val ENUM = "enum"
+    const val EXTENDS = "extends"
+    const val FINAL = "final"
+    const val FINALLY = "finally"
+    const val FLOAT = "float"
+    const val FOR = "for"
+    const val IF = "if"
+    const val IMPLEMENTS = "implements"
+    const val IMPORT = "import"
+    const val INSTANCEOF = "instanceof"
+    const val INT = "int"
+    const val INTERFACE = "interface"
+    const val LONG = "long"
+    const val NATIVE = "native"
+    const val NEW = "new"
+    const val PACKAGE = "package"
+    const val PRIVATE = "private"
+    const val PROTECTED = "protected"
+    const val PUBLIC = "public"
+    const val RETURN = "return"
+    const val SHORT = "short"
+    const val STATIC = "static"
+    const val STRICTFP = "strictfp"
+    const val SUPER = "super"
+    const val SWITCH = "switch"
+    const val SYNCHRONIZED = "synchronized"
+    const val THIS = "this"
+    const val THROW = "throw"
+    const val THROWS = "throws"
+    const val TRANSIENT = "transient"
+    const val TRY = "try"
+    const val VOID = "void"
+    const val VOLATILE = "volatile"
+    const val WHILE = "while"
+}
+
+val javaKeywords = listOf(
+    JavaKeywordConstants.ABSTRACT, JavaKeywordConstants.ASSERT, JavaKeywordConstants.BOOLEAN,
+    JavaKeywordConstants.BREAK, JavaKeywordConstants.BYTE, JavaKeywordConstants.CASE,
+    JavaKeywordConstants.CATCH, JavaKeywordConstants.CHAR, JavaKeywordConstants.CLASS,
+    JavaKeywordConstants.CONTINUE, JavaKeywordConstants.DEFAULT, JavaKeywordConstants.DO,
+    JavaKeywordConstants.DOUBLE, JavaKeywordConstants.ELSE, JavaKeywordConstants.ENUM,
+    JavaKeywordConstants.EXTENDS, JavaKeywordConstants.FINAL, JavaKeywordConstants.FINALLY,
+    JavaKeywordConstants.FLOAT, JavaKeywordConstants.FOR, JavaKeywordConstants.IF,
+    JavaKeywordConstants.IMPLEMENTS, JavaKeywordConstants.IMPORT, JavaKeywordConstants.INSTANCEOF,
+    JavaKeywordConstants.INT, JavaKeywordConstants.INTERFACE, JavaKeywordConstants.LONG,
+    JavaKeywordConstants.NATIVE, JavaKeywordConstants.NEW, JavaKeywordConstants.PACKAGE,
+    JavaKeywordConstants.PRIVATE, JavaKeywordConstants.PROTECTED, JavaKeywordConstants.PUBLIC,
+    JavaKeywordConstants.RETURN, JavaKeywordConstants.SHORT, JavaKeywordConstants.STATIC,
+    JavaKeywordConstants.STRICTFP, JavaKeywordConstants.SUPER, JavaKeywordConstants.SWITCH,
+    JavaKeywordConstants.SYNCHRONIZED, JavaKeywordConstants.THIS, JavaKeywordConstants.THROW,
+    JavaKeywordConstants.THROWS, JavaKeywordConstants.TRANSIENT, JavaKeywordConstants.TRY,
+    JavaKeywordConstants.VOID, JavaKeywordConstants.VOLATILE, JavaKeywordConstants.WHILE
+)
--- src/main/kotlin/SourceFileProcessor.kt
+++ src/main/kotlin/SourceFileProcessor.kt
@@ -1,102 +1,53 @@
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+import java.io.IOException
 import java.io.InputStream
+import kotlin.io.path.*
 
-object JavaKeywordConstants {
-    const val ABSTRACT = "abstract"
-    const val ASSERT = "assert"
-    const val BOOLEAN = "boolean"
-    const val BREAK = "break"
-    const val BYTE = "byte"
-    const val CASE = "case"
-    const val CATCH = "catch"
-    const val CHAR = "char"
-    const val CLASS = "class"
-    const val CONTINUE = "continue"
-    const val DEFAULT = "default"
-    const val DO = "do"
-    const val DOUBLE = "double"
-    const val ELSE = "else"
-    const val ENUM = "enum"
-    const val EXTENDS = "extends"
-    const val FINAL = "final"
-    const val FINALLY = "finally"
-    const val FLOAT = "float"
-    const val FOR = "for"
-    const val IF = "if"
-    const val IMPLEMENTS = "implements"
-    const val IMPORT = "import"
-    const val INSTANCEOF = "instanceof"
-    const val INT = "int"
-    const val INTERFACE = "interface"
-    const val LONG = "long"
-    const val NATIVE = "native"
-    const val NEW = "new"
-    const val PACKAGE = "package"
-    const val PRIVATE = "private"
-    const val PROTECTED = "protected"
-    const val PUBLIC = "public"
-    const val RETURN = "return"
-    const val SHORT = "short"
-    const val STATIC = "static"
-    const val STRICTFP = "strictfp"
-    const val SUPER = "super"
-    const val SWITCH = "switch"
-    const val SYNCHRONIZED = "synchronized"
-    const val THIS = "this"
-    const val THROW = "throw"
-    const val THROWS = "throws"
-    const val TRANSIENT = "transient"
-    const val TRY = "try"
-    const val VOID = "void"
-    const val VOLATILE = "volatile"
-    const val WHILE = "while"
-}
+@Serializable
+data class SourceFileStats(val isTestFile: Boolean, val counter: Map<String, Int>)
 
-val javaKeywords = listOf(
-    JavaKeywordConstants.ABSTRACT, JavaKeywordConstants.ASSERT, JavaKeywordConstants.BOOLEAN,
-    JavaKeywordConstants.BREAK, JavaKeywordConstants.BYTE, JavaKeywordConstants.CASE,
-    JavaKeywordConstants.CATCH, JavaKeywordConstants.CHAR, JavaKeywordConstants.CLASS,
-    JavaKeywordConstants.CONTINUE, JavaKeywordConstants.DEFAULT, JavaKeywordConstants.DO,
-    JavaKeywordConstants.DOUBLE, JavaKeywordConstants.ELSE, JavaKeywordConstants.ENUM,
-    JavaKeywordConstants.EXTENDS, JavaKeywordConstants.FINAL, JavaKeywordConstants.FINALLY,
-    JavaKeywordConstants.FLOAT, JavaKeywordConstants.FOR, JavaKeywordConstants.IF,
-    JavaKeywordConstants.IMPLEMENTS, JavaKeywordConstants.IMPORT, JavaKeywordConstants.INSTANCEOF,
-    JavaKeywordConstants.INT, JavaKeywordConstants.INTERFACE, JavaKeywordConstants.LONG,
-    JavaKeywordConstants.NATIVE, JavaKeywordConstants.NEW, JavaKeywordConstants.PACKAGE,
-    JavaKeywordConstants.PRIVATE, JavaKeywordConstants.PROTECTED, JavaKeywordConstants.PUBLIC,
-    JavaKeywordConstants.RETURN, JavaKeywordConstants.SHORT, JavaKeywordConstants.STATIC,
-    JavaKeywordConstants.STRICTFP, JavaKeywordConstants.SUPER, JavaKeywordConstants.SWITCH,
-    JavaKeywordConstants.SYNCHRONIZED, JavaKeywordConstants.THIS, JavaKeywordConstants.THROW,
-    JavaKeywordConstants.THROWS, JavaKeywordConstants.TRANSIENT, JavaKeywordConstants.TRY,
-    JavaKeywordConstants.VOID, JavaKeywordConstants.VOLATILE, JavaKeywordConstants.WHILE
-)
-
-val delimiters = listOf(
-    "\b", "\t", "\n", "\r", " ",
-    "!", "\"", "#", "$", "%",
-    "&", "'", "(", ")", "*",
-    "+", ",", "-", ".", "/",
-    ":", ";", "<", "=", ">",
-    "?", "@", "`", "[", "\\",
-    "]", "^", "{", "|", "}",
-    "~"
-)
-
-class SourceFileProcessor {
+class SourceFileProcessor(pathToCacheFolder: String) {
+    private val cacheFolder = Path(pathToCacheFolder)
     private val counter = javaKeywords.associateWith { 0 }.toMutableMap()
-    private val delimiterRegexpString = """ !"#$%&'()*+,-./:;<=>?@`\[\]^{|}~\\"""
-    private val delimiterRegex = Regex("(?<=[$delimiterRegexpString])|(?=[$delimiterRegexpString])")
     private var commentDepth = 0 // depth of current comment section
                                  // needed to correctly process nested comments
+
+    companion object {
+        private const val delimiterRegexpString = """ !"#$%&'()*+,-./:;<=>?@`\[\]^{|}~\\"""
+        private val delimiterRegex = Regex("(?<=[$delimiterRegexpString])|(?=[$delimiterRegexpString])")
+    }
+
+    init {
+        if (!cacheFolder.exists() || !cacheFolder.isDirectory() || !cacheFolder.isWritable()) {
+            throw IOException("Cache folder doesn't exist or is not writeable")
+        }
+    }
+
+    /*fun run(sourceFilePath: String) {
+        val cachedFile = cacheFolder / sourceFilePath.replace("/", "+++")
+        if (cachedFile.exists()) {
+
+        }
+    }*/
 
     fun processSourceFile(sourceFile: InputStream) {
         val sourceStream = sourceFile.bufferedReader().lines()
         sourceStream.forEach { line ->
             processLine(line)
         }
+    }
 
-        counter.forEach { (kw, cnt) ->
-            println("'$kw': $cnt")
+    fun saveStatsToFile(fileName: String, isTestFile: Boolean = false) {
+        val filePath = cacheFolder / fileName
+        if (isTestFile) {
+            filePath.writeText(Json.encodeToString(SourceFileStats(true, mapOf())))
+            return
         }
+
+        filePath.writeText(Json.encodeToString(SourceFileStats(false, counter)))
     }
 
     private fun updateCommentDepth(token1: String, token2: String) {
